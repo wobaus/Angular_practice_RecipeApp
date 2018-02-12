cfe75a474c5f7a3785d44293bdb17adfe717ce75
Sun Baus <wobaus@gmail.com>
2018-02-12 15:36:57 -0800

    complete basic set up

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -7,9 +7,10 @@ import { Recipe } from  '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipee', 'This is simply a test', 'https://www.google.com/imgres?imgurl=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2F1506120378%2FMR_0917170472.jpg%3Fitok%3DKPTNrvis&imgrefurl=http%3A%2F%2Fwww.myrecipes.com%2Frecipe%2Flemony-chicken-and-spinach-soup&docid=frbyewjXQOHF-M&tbnid=W98drh1pADRaLM%3A&vet=10ahUKEwjZ-MOY_KDZAhUUUGMKHTPoD8YQMwikAigAMAA..i&w=1200&h=900&client=firefox-b-ab&bih=585&biw=1040&q=recipe&ved=0ahUKEwjZ-MOY_KDZAhUUUGMKHTPoD8YQMwikAigAMAA&iact=mrc&uact=8')
-  ];
+  recipes: Recipe[ ] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+ ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
+import { I18NHtmlParser } from '@angular/compiler/src/i18n/i18n_html_parser';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredients[] = [
+    new Ingredients ('Apple',5),
+    new Ingredients ('Tomato', 10)
+  ];
   constructor() { }
 
   ngOnInit() {
