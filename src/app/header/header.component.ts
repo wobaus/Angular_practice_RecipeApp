6f45eee300f0541c8e1a56859ed4d9fa7f2b2d63
Sun Baus <wobaus@gmail.com>
2018-02-15 10:00:08 -0800

    starting databinding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, Output,EventEmitter } from "@angular/core";
+// import { EventEmitter } from "@angular/core/src/event_emitter";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() featureSelected = new EventEmitter<string>();
+
+    onSelect(feature: string){
+        this.featureSelected.emit(feature);    
+    }
 
 }
--- src/app/recipe-list/recipes-item/recipes-item.component.ts
+++ src/app/recipe-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipes/recipe.model';
 
 @Component({
   selector: 'app-recipes-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-item.component.css']
 })
 export class RecipesItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+
 
   constructor() { }
 
